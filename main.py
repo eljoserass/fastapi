8d5f54386a7dc6b128bcfc69d6a6d51786d66194
eljoserass <jose-antonio.rodriguez-assalone@epitech.eu>
2024-10-24 17:38:51 +0200

    fix. removing single quote issue

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ MYSQL_DB = os.getenv("MYSQLDATABASE")
 MYSQL_PORT = os.getenv("MYSQLPORT")
 MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
 MYSQL_CONNECTOR = "mysql+mysqlconnector"
-DATABASE_URL = f"{MYSQL_CONNECTOR}://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+DATABASE_URL = f"{MYSQL_CONNECTOR.replace("'", "")}://{MYSQL_USER.replace("'", "")}:{MYSQL_PASSWORD.replace("'", "")}@{MYSQL_HOST.replace("'", "")}:{MYSQL_PORT.replace("'", "")}/{MYSQL_DB.replace("'", "")}"
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)

7a6987b6742514f5fe6efd057a4d4223d18c73db
eljoserass <jose-antonio.rodriguez-assalone@epitech.eu>
2025-03-17 18:09:42 +0100

    removed old prompts

--- language.py
+++ language.py
@@ -4,23 +4,6 @@ from src.backend.models import Message
 from openai import OpenAI
 from pydantic import BaseModel
 
-
-SYSTEM = """Eres un asistente de recambista en España. Tu labor sera leer las conversaciones con clientes en Whatsapp y simplificar su entendimiento al recambista
-Para hacer esto, leeras la conversacion entre Toni (el mecanico pidiendo recambios) y Guillem (el recambista). El usuario te enviara la conversacion.
-De esta conversacion tienes que:
-- identificar las distintas ordenes de Toni
-- una vez identificada extraer la informacion que Toni quiere saber sobre ese pedido
-- la informacion se puede contener en forma de imagenes.
-- por cada pedido indetifica modelo, marca, placa del vehiculo y lo que toni esta pidiendo sobre este."""
-
-SYSTEM_PRO = """You will receive messages in spanish. This messages are from a mechanic, making an order.
-Parse the message into the order. In some cases you will receive messages that all refer to the same order, and in others, it can be multiple orders.
-An order can be only about one car, so each order can only have one car plate.
-The messages might contain images that are used to give information about the car plate or about the car part that is being order. Reference the image used for your answer.
-Answer always in spanish.
-You will receive multiple messages, so remember that you might need to parse the messages into multiple orders
-"""
-
 SYSTEM_DEFAULT = "You are an expert at structured data extraction. You will be given unstructured text from a chat with a mechanic asking for quotas on car parts and should convert it into the given structure."
 
 def encode_image(image_path):
@@ -67,7 +50,7 @@ async def message_to_orders(messages: list[Message]):
     return completion
 
 
-async def call_llm(messages: list[Message]): # -> list[Order] 
+async def call_llm(messages: list[Message]): # -> list[Order]
 
     completion = await message_to_orders(messages)
     if hasattr(completion, 'choices') and len(completion.choices) > 0:
